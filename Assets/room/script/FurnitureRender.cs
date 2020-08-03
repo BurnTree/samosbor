759149f1682aaed8491592b2211bb1f22e75fa72
ilya.Ye <dream.sun.1944@gmail.com>
2020-08-04 00:01:31 +0300

    add meta add generators

---
+++ Assets/room/script/FurnitureRender.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FurnitureRender : MonoBehaviour
+{
+    public GameObject furnitures;
+    private BoxCollider2D _collider2D;
+
+    void Start()
+    {
+        _collider2D = GetComponent<BoxCollider2D>();
+        Instantiate(furnitures, new Vector2(transform.position.x, _collider2D.bounds.min.y),
+            Quaternion.identity);
+    }
+}
---
+++ Assets/room/script/RoomRender.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RoomRender : MonoBehaviour
+{
+    public GameObject room;
+    void Start()
+    {
+        Instantiate(room, new Vector2(0f, 0f), Quaternion.identity);
+    }
+}
