87c3a612169b06af39e16ad64ee5385f1adff760
ilya.Ye <dream.sun.1944@gmail.com>
2020-08-11 23:01:11 +0300

    add moving camera to floor scene

---
+++ Assets/floor/script/MovingCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+    private Vector3 touchStart;
+
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 5;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPosition = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPosition = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchOnePrevPosition - touchZeroPrevPosition).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+            zoom(difference + 0.01f);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+}
